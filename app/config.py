337be45ed5f10a86b025c8ae3d6095b66ebb3acc
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-02-13 12:48:53 +0400

    refactoring config.py

--- app/config.py
+++ app/config.py
@@ -7,19 +7,21 @@ models = [
 ]
 
 
-class Config:
-    DEBUG = False
-    CSRF_ENABLED = True
+class BaseConfig:
     WTF_CSRF_SECRET_KEY = 'dsofpkoasodksap'
     SECRET_KEY = 'zxczxasdsad'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-class ProductionConfig(Config):
+class ProductionConfig(BaseConfig):
     DEBUG = False
+    CSRF_ENABLED = True
 
 
-class DevelopConfig(Config):
+class DevelopConfig(BaseConfig):
     DEBUG = True
     ASSETS_DEBUG = True
+
+
+Config = ProductionConfig
