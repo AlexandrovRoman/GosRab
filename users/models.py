a1de11450b5a70fab8de2523cc1b216af356c5cc
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-02-13 14:44:43 +0400

    Add registration

--- users/models.py
+++ users/models.py
@@ -1,20 +1,21 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,
-                           primary_key=True, autoincrement=True)
+                   primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=True)
     surname = db.Column(db.String(80), nullable=True)
     fathername = db.Column(db.String(80), nullable=True)
     email = db.Column(db.String(40),
-                              index=True, unique=True, nullable=True)
+                      index=True, unique=True, nullable=True)
     hashed_password = db.Column(db.String, nullable=True)
     # birth_date = db.Column(db.Date, nullable=True)
     status = db.Column(db.String, nullable=True,
-                               default='standart_user')  # standart_user, admin, organistaion, superuser
+                       default='standart_user')  # standart_user, admin, organistaion, superuser
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,7 +25,6 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
-
 
 # session = create_session()
 # for i in range(1, 5):
