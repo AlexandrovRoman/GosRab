9a972195758f0c2f13e60879f58bae39c59d274d
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-02-13 11:57:59 +0400

    Вроде пофиксил

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from warnings import warn
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -11,3 +12,6 @@ migrate = Migrate(app, db)
 
 def add_urls():
     from app import urls
+    if len(urls.urlpatterns) == 0:
+        warn("urlpatterns is empty")
+    assert all(urls.urlpatterns)
