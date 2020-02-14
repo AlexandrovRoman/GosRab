3592e73ccfb07a1ffa12dbcb483f77c33201ef23
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-02-14 11:13:22 +0400

    push var to html

--- errors/views.py
+++ errors/views.py
@@ -4,9 +4,9 @@ from app import app
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html')
+    return render_template('404.html', error=404)
 
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template('500.html')
+    return render_template('500.html', error=500)
