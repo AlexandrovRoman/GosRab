3770daf223823a5d5c51c30668daa7b13be4b3c8
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-02-29 13:44:56 +0400

    Изменил структуру templates т.к. она начала скатываться в говно

--- utils/urls.py
+++ utils/urls.py
@@ -2,40 +2,40 @@ from app import app
 from importlib import import_module
 from warnings import warn
 
-
 debug = app.debug
+
+
+def add_url(url, func, methods):
+    if not (isinstance(url, str) and hasattr(func, '__call__')):
+        warn(f"Некорректные данные", Warning)
+        return False
+    app.add_url_rule(url, view_func=func, methods=methods)
+    print(f'add url: {url}\n' if debug else '', end='')
+    return True
 
 
 def path(url: str, func, *, methods=['GET'], _main=False):
     if not hasattr(func, '__iter__'):
         if _main:
-            if not (isinstance(url, str) and hasattr(func, '__call__')):
-                warn(f"Некорректные данные", Warning)
-                return False
-            app.add_url_rule(url, view_func=func, methods=methods)
-            print(f'add url: {url}\n' if debug else '', end='')
-            return True
+            return add_url(url, func, methods)
         else:
             return url, func, methods
     else:
         for url_, fnc, methods_ in func:
-            if not (isinstance(url_, str) and hasattr(fnc, '__call__')):
-                warn(f"Некорректные данные", Warning)
+            if not add_url(url + url_, fnc, methods_):
                 return False
-            app.add_url_rule(url + url_, view_func=fnc, methods=methods_)
-            print(f'add url: {url + url_}\n' if debug else '', end='')
         return True
 
 
 def include(dir):
     try:
         pack = import_module(dir)
-        try:
-            patterns = getattr(pack, 'urlpatterns')
-        except AttributeError:
-            raise AttributeError(f'Файл {dir}.urlpatterns не содержит urlpatterns')
     except ModuleNotFoundError:
         raise ImportError(f"Не удалось найти {dir}.urlpatterns")
+    try:
+        patterns = getattr(pack, 'urlpatterns')
+    except AttributeError:
+        raise AttributeError(f'Файл {dir}.urlpatterns не содержит urlpatterns')
     print('include success\n' if debug else '', end='')
     for args in patterns:
         if isinstance(args, bool):
