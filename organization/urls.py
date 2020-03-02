52ab3ef90911aa9bff8aba773d3a4ee33b69e61c
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-03-02 20:59:15 +0400

    Change urls in html

--- organization/urls.py
+++ organization/urls.py
@@ -4,8 +4,8 @@ from .views import organization, add_organization, organizations, menu_organizat
 # Add your urls
 urlpatterns = [
     path('job/', job),
-    path('profile/personnel/organization', organization),
-    path('profile/organizations', organizations),
-    path('profile/redact/add_organization', add_organization),
+    path('profile/personnel/organization/', organization),
+    path('profile/organizations/', organizations),
+    path('profile/redact/add_organization/', add_organization),
     path('profile/menu_organization/', menu_organization),
 ]
