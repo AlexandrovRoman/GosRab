2068e89aff3a3bcf6e0bf4ccdb80f30296a272fb
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2020-03-11 09:10:32 +0400

    Create prepare_db.py

---
+++ prepare_db.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+import sys
+
+
+def confirm(msg):
+    ans = input(f'\n\t{msg}. Наберите "n", для выключения, enter для продолжения.')
+    if ans == 'n':
+        sys.exit()
+    print()
+    
+    
+parser = argparse.ArgumentParser()
+parser.add_argument("pythonenv")
+
+args = parser.parse_args()
+
+os.system(f'{args.pythonenv} manage.py db init')
+confirm('manage.py db init завершено.')
+os.system(f'{args.pythonenv} manage.py db migrate')
+confirm('manage.py db migrate завершено.')
+os.system(f'{args.pythonenv} manage.py db upgrade')
+confirm('manage.py db upgrade завершено.')
+os.system(f'{args.pythonenv} set_roles.py')
+confirm('set_roles.py завершено.')
+os.system(f'{args.pythonenv} excel-DB.py')
+print('\n\texcel-DB.py завершено.\n')
