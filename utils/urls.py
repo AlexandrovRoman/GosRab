44119a1be52d2bb59f7f2e010724d388afa66fcc
AlexandrovRoman <AlexandrovRomanisi@ya.ru>
2020-03-16 15:57:55 +0400

    Rename path to _path

--- utils/urls.py
+++ utils/urls.py
@@ -2,8 +2,8 @@
 from app import app
 from Flask_DJ import urls
 
-path = urls.Path(app)
-add_absolute_path = path.add_absolute_path
-add_relative_path = path.add_relative_path
-relative_path = path.relative_path
+_path = urls.Path(app)
+add_absolute_path = _path.add_absolute_path
+add_relative_path = _path.add_relative_path
+relative_path = _path.relative_path
 include = urls.include
