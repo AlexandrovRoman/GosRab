8c991e01d6377cb33f4d3c16996cf0dd9c2ac186
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2020-03-16 15:44:40 +0400

    Fix prepare_db.py

--- prepare_db.py
+++ prepare_db.py
@@ -1,6 +1,11 @@
 import os
 import argparse
 import sys
+from news.models import News, Courses
+from organization.models import Organization
+from users.models import User
+from utils.excel_DB import export_from_excel
+from utils.set_roles import create_roles
 
 
 def confirm(msg):
@@ -8,8 +13,15 @@ def confirm(msg):
     if ans == 'n':
         sys.exit()
     print()
-    
-    
+
+
+def create_test_models():
+    export_from_excel('test_models/orgs.xlsx', Organization.new)
+    export_from_excel('test_models/users.xlsx', User.new)
+    export_from_excel('test_models/news.xlsx', News.new)
+    export_from_excel('test_models/courses.xlsx', Courses.new)
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("pythonenv")
 
@@ -21,7 +33,7 @@ os.system(f'{args.pythonenv} manage.py db migrate')
 confirm('manage.py db migrate завершено.')
 os.system(f'{args.pythonenv} manage.py db upgrade')
 confirm('manage.py db upgrade завершено.')
-os.system(f'{args.pythonenv} scripts/set_roles.py')
+create_roles()
 confirm('set_roles.py завершено.')
-os.system(f'{args.pythonenv} excel-DB.py')
-print('\n\texcel-DB.py завершено.\n')
+create_test_models()
+print('\n\texcel_DB.py завершено.\n')
---
+++ utils/set_roles.py
@@ -0,0 +1,13 @@
+from users.models import Role
+from app import session
+
+
+def create_roles():
+    session.query(Role).delete()
+    Role.new('user', 'peasant')
+    Role.new('hr manager', 'управляет кадрами организации')
+    Role.new('company director', 'может управлять организацией, создавать отделы')
+    Role.new('head of department', 'глава отдела')
+    Role.new('admin', 'доступны фичи админа: .......')
+    Role.new('superuser', 'доступны фичи суперюзера: .........')
+    session.commit()
