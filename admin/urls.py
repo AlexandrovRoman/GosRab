f3403783bc722940a37799c7ced790f8f6b02d33
AlexandrovRoman <alexandrovromanisi@ya.ru>
2020-06-20 15:06:56 +0400

    delete unused files

--- admin/urls.py
+++
@@ -1,8 +0,0 @@
-# from flask_admin import Admin
-# from app import db, app
-# from .models import *
-# from users.models import User
-
-# admin = Admin(app, 'Главная страница', url='/', index_view=HomeAdminView(name='Home'))
-# admin.add_view(NewsView(model=Model1, session=db.session, name='Новости', roles_accepted=['admin']))
-# admin.add_view(NewsView(model=User, session=db.session, name='Статистика', roles_accepted=['superuser']))
